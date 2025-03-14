5e1fa4b61510bcaa7927ab2ee1887aad8cb1ce72
Vallor23 <enosnyabinda2017@gmail.com>
2025-03-14 10:45:21 +0300

    pattern drawing using conditional loops

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,8 @@
 size = int(input("Enter the size of the pattern:"))
-while size:
+while size <= 0:
+    print("Please enter a positive number.")
+else:
     for i in range(size):
-        print("*", end="")
-    print()
+        for j in range(size):
+            print("*", end="")
+        print()
