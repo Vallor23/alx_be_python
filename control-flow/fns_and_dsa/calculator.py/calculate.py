6a0ce0a0c825d33fe039eeecf941a6d774b86564
Vallor23 <enosnyabinda2017@gmail.com>
2025-03-24 17:08:24 +0300

    : Creating and Using Modules

---
+++ control-flow/fns_and_dsa/calculator.py/calculate.py
@@ -0,0 +1,18 @@
+def add(x, y):
+#   Returns the sum of two numbers.
+  return x + y
+
+def subtract(x, y):
+#   Returns the difference of two numbers.
+  return x - y
+
+def multiply(x, y):
+#   Returns the product of two numbers.
+  return x * y
+
+def divide(x, y):
+#   Returns the quotient of two numbers.
+  if y == 0:
+    raise ValueError('Cannot divide by zero!')
+  return x / y
+
---
+++ control-flow/fns_and_dsa/calculator.py/main.py
@@ -0,0 +1,7 @@
+import calculate
+
+print(calculate.add(1, 2))
+print(calculate.subtract(1, 2))
+print(calculate.multiply(1, 2))
+print(calculate.divide(1, 2))
+print(calculate.divide(1, 0))
