4a8381112ca1dbf5407727b77d4c872d6917ebe0
Vallor23 <enosnyabinda2017@gmail.com>
2025-03-29 06:48:25 +0300

    mplements a simple interface for managing a shopping list

---
+++ control-flow/fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,37 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+    
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ").strip()
+
+        if choice == "1":
+            item = input("Enter the item: ").strip()
+            shopping_list.append(item)
+        elif choice == "2":
+            item = input("Enter the item: ").strip()
+            if item in shopping_list:
+                shopping_list.remove(item)
+            else:
+                print("Item not found")
+        elif choice == "3":
+            print("Shopping List")
+            for item in shopping_list:
+                print(item)
+        elif choice == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice.Please try again")
+            
+if __name__ == "__main__":
+    main()
+
+
+    
