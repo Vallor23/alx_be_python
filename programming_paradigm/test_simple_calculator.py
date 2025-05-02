b236b05313e875e2c8165fa4a7a17579a5304da2
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-02 17:49:19 +0300

    Writing comprehensive tests

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,16 +13,19 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         
     def test_subtraction(self):
-        """Test the addition method."""
+        """Test the subtraction method."""
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
         
     def test_multiplication(self):
-        """Test the addition method."""
+        """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
         
     def test_division(self):
-        """Test the addition method."""
+        """Test the division method."""
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertEqual(self.calc.divide(-1, 1), -1)
+
+if __name__ == "__main__":
+    unittest.main()
